f81639f3b773541c784a245ca8c2dc6076a8f3cf
Piotr Pawełczak <pecio@student.agh.edu.pl>
2022-05-24 11:54:08 +0200

    Chat style

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,13 @@
-# chat/consumers.py
-import json
-from channels.generic.websocket import WebsocketConsumer
-
-
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-
+from django.utils import timezone
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        self.user = self.scope['user']
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -32,21 +28,28 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat()
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
+        user = event['user']
+        datetime = event['datetime']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user': user,
+            'datetime': datetime,
         }))
